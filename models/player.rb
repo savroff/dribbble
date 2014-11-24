15dee7ae46d4b48de5e01d517cd1f421ba834432
savroff <savroff@gmail.com>
2014-11-25 01:33:28 +0400

    init commit

---
+++ models/player.rb
@@ -0,0 +1,13 @@
+class Player
+  include Virtus.model
+
+  #player info
+  attribute :first_name, String
+  attribute :last_name, String
+  attribute :age, Integer
+  attribute :salary, BigDecimal
+
+  #player skills
+
+
+end
